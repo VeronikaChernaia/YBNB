a1b694b6ee36bad0f88a19cf305b2a4018e38950
Veronika Chernaia <veronika.chernaia75@gmail.com>
2024-08-27 22:24:57 +0200

    Added the incoming booking requests to the dashboard, added book buttons to the yachts index page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,11 @@
 class BookingsController < ApplicationController
   before_action :set_yacht, only: [:new, :create]
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+
+
+
+
   def new
     @yacht = Yacht.find(params[:yacht_id])
     @booking = Booking.new
@@ -13,13 +18,29 @@ class BookingsController < ApplicationController
     @booking.user = current_user
     @booking.save
     if @booking.save!
-    redirect_to yachts_path(@yacht)
+    redirect_to dashboard_path(@yacht), notice: 'Your booking request has been submitted.'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+
+  def update
+    if @booking.update(booking_params)
+      redirect_to dashboard_path, notice: 'Booking status updated successfully.'
+    else
+      render :show, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_booking
+    @booking = Booking.find_by(yacht_id: params[:yacht_id], id: params[:id])
+    if @booking.nil?
+      redirect_to dashboard_path, alert: "Booking not found."
+    end
+  end
 
   def set_yacht
     @yacht = Yacht.find(params[:yacht_id])

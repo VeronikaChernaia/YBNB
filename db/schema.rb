ec34a44a62f500fbde860848814447270ae8e672
Ariana Bakhshandeh <arianabakhshandeh@gmail.com>
2024-08-29 11:23:56 +0100

    map feature added to index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_27_195828) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_28_123306) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -102,6 +102,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_27_195828) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.integer "price_per_hour"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_yachts_on_user_id"
   end
 

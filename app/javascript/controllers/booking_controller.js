bab59b7b17b8fd350b11ffa47bca3683d15a1b73
Benjamin Heymann <benjamin@bretler.ch>
2024-08-30 08:48:26 +0200

    added btn validation for booking

--- app/javascript/controllers/booking_controller.js
+++ app/javascript/controllers/booking_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["checkIn", "checkOut", "price", "pricePerDay"];
+  static targets = ["checkIn", "checkOut", "price", "pricePerDay", "btn"];
 
   connect() {
     console.log();
@@ -13,7 +13,9 @@ export default class extends Controller {
 
     if (checkInDate && checkOutDate) {
       if (new Date(checkInDate) > new Date(checkOutDate)) {
-        price.innerText = "The date is not correct";
+        this.priceTarget.innerText = "The date is in the past";
+        this.btnTarget.disabled = true;
+        this.priceTarget.style.color = "red";
         return;
       }
       this.calculatePrice(checkInDate, checkOutDate);
@@ -21,13 +23,15 @@ export default class extends Controller {
   }
   calculatePrice(checkIn, checkOut) {
     const price = this.calculatePlaceholderPrice(checkIn, checkOut);
-    this.priceTarget.textContent = `Your price: $${price}`;
+    this.priceTarget.textContent = `Your price : $${price}`;
   }
 
   calculatePlaceholderPrice(checkIn, checkOut) {
     const start = new Date(checkIn);
     const end = new Date(checkOut);
     const days = Math.abs(end - start) / (1000 * 60 * 60 * 24);
-    return days * pricePerDay.innerText; // Assuming $100 per day
+    this.priceTarget.style.color = "green";
+    this.btnTarget.disabled = false;
+    return days * pricePerDay.innerText;
   }
 }
